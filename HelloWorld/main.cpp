fcd8dfb235bbb21b4d6bf0de6f1e86dfc9c04786
cheewing <cheewing@cheewingdembp.lan>
2014-09-11 01:06:37 +0800

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by cheewing on 14-9-11.
+//  Copyright (c) 2014年 cheewing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
